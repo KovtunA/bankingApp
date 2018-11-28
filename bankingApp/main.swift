7e024d8bd425317a6780caef6658885a39b80421
Anastasia <anastasia@macbook-pro-3.local>
2018-11-28 13:01:39 +0200

    Initial Commit

---
+++ bankingApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bankingApp
+//
+//  Created by Anastasia on 11/28/18.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
