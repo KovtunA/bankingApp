cf635890828b8c025dd67d87738ee4745267149e
Anastasia <anastasia@MacBook-Pro-3.local>
2018-12-11 17:02:43 +0200

    accounts arrey

--- bankingApp/Storage.swift
+++ bankingApp/Storage.swift
@@ -27,6 +27,26 @@ func accsessToUsers(contentsOfFile: String) {
 //guard let infoFromfile=infoFromfile else {print("Smth wrong wit infoFromFile")}
 
 
+var accountsArrey: [Account] = []
+let infoFromfile = contentsOfFile.split(separator: "\n")
+let infoFromfileMaped: [String] = infoFromfile.map { str in String(str) }
+
+//var arrayOfArrays: [[String]] = []
+
+for str in infoFromfileMaped {
+    let aLine = str.components(separatedBy: ",")
+    let name = String (aLine[0])
+    guard let amount = Double (aLine[1]),
+        let changes = Double (aLine[2]),
+        let newResult = Double (aLine[3]) else { print("Smth wrong with optional"); continue }
+    let account = Account(name: name, account: amount, changes: changes, amount: newResult)
+    accountsArrey.append(contentsOf: account)
+}
+
+print(accountsArrey)
+
+
+
 //let line = randomNames.сmap { aLine:() in}
 //    let surnameLastnameAge = aLine.replacingOccurrences(of: "\"", with: "").split(separator:",")
 //    guard surnameLastnameAge.count == 3, let age = Int(surnameLastnameAge[2]) else { print("Smth went wrong")
